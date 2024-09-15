8ed3247ba59a4722bab686cf39600d982366be7b
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-15 20:44:34 +0600

    Update A. Simple Palindrome.cpp

--- Codeforces Round 972 (Div. 2)/A. Simple Palindrome.cpp
+++ Codeforces Round 972 (Div. 2)/A. Simple Palindrome.cpp
@@ -1,1 +1,27 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int n;
+string help = "aeiou";
+ 
+void solve() {
+  cin >> n;
+  int k = (n + 4) / 5;
+  string ans = "";
+  for (int i = 0; i < k; i++)
+    for (int j = 0; j < 5; j++)
+      ans += help[j];   
+  while (ans.size() > n)
+    ans.pop_back();
+  sort(ans.begin(), ans.end());
+  cout << ans << endl;
+}
+ 
+int main() {
+  int t;
+  cin >> t;
+  while (t--)
+    solve();
+   return 0;
+}
