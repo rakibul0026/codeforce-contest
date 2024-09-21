fe6a6b757b24d5b45117612ebb1860dec5abd464
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-22 00:05:54 +0600

    Update B. Robin Hood and the Major Oak.cpp

--- Codeforces Round 974 (Div. 3)/B. Robin Hood and the Major Oak.cpp
+++ Codeforces Round 974 (Div. 3)/B. Robin Hood and the Major Oak.cpp
@@ -40,3 +40,26 @@ NO
 NO
 YES*/
 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t; 
+    while (t--) {
+        long long n, k;
+        cin >> n >> k; 
+        long long sum = k*(2*n-k+1)/2;
+        
+   if (sum % 2 == 0) {
+            cout << "YES" << endl;
+        } 
+        
+        else {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
+
+
