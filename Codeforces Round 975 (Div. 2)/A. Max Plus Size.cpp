fe4862ca90cf0cee44e33848de56f16f51165208
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-28 12:47:41 +0600

    Update A. Max Plus Size.cpp

--- Codeforces Round 975 (Div. 2)/A. Max Plus Size.cpp
+++ Codeforces Round 975 (Div. 2)/A. Max Plus Size.cpp
@@ -1,1 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t; 
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++) {
+            cin >> a[i]; 
+        }
+
+      
+        int maxEven = 0, maxOdd = 0;
+        int countEven = 0, countOdd = 0;
+
+      
+        for (int i = 0; i < n; i += 2) {
+           
+            maxEven = max(maxEven, a[i]);
+            countEven++;
+        }
+
+        for (int i = 1; i < n; i += 2) {
+          
+            maxOdd = max(maxOdd, a[i]);
+            countOdd++;
+        }
+
+       
+        int scoreEven = maxEven + countEven;
+        int scoreOdd = maxOdd + countOdd;
+
+       
+        cout << max(scoreEven, scoreOdd) << endl;
+    }
+    return 0;
+}
 
