3a242cbf2f84fbb7433faacfb2a68646b16067f4
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-09-30 00:29:07 +0600

    Update A. Find Minimum Operations.cpp

--- Codeforces Round 976 (Div. 2) and Divide By Zero 9.0 /A. Find Minimum Operations.cpp
+++ Codeforces Round 976 (Div. 2) and Divide By Zero 9.0 /A. Find Minimum Operations.cpp
@@ -1,1 +1,32 @@
 
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    long long t;
+    cin >> t; 
+    while (t--) {
+        long long n, k;
+        cin >> n >> k; 
+ 
+        long long operations = 0;
+ 
+        
+        if (k == 1) {
+            operations = n; 
+        } else {
+           
+            while (n > 0) {
+                operations += n % k; 
+                n /= k;
+            }
+        }
+ 
+        cout << operations << '\n'; 
+    }
+ 
+    return 0;
+}
