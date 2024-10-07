ff76347f66729ebc8ee704129fb3d7a57ad7aa92
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-07 22:51:31 +0600

    Update p1.cpp

--- Codeforces Round 977 (Div. 2, based on COMPFEST 16 - Final Round)/p1.cpp
+++ Codeforces Round 977 (Div. 2, based on COMPFEST 16 - Final Round)/p1.cpp
@@ -1,1 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        priority_queue<int, vector<int>, greater<int>> pq;
+
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            pq.push(x);
+        }
+
+        while (pq.size() > 1)
+        {
+            int x = pq.top();
+            pq.pop();
+            int y = pq.top();
+            pq.pop();
+
+            int new_element = (x + y) / 2;
+            pq.push(new_element);
+        }
+
+        cout << pq.top() ;
+        cout<< endl;
+    }
+    return 0;
+}
 
