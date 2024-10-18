a81dbe8ea93983c7439e032ca04f3a356f8950af
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-18 20:01:38 +0600

    Update A.cpp

--- ICPC Collaborative Contest - 82 (ICPC Preli Edition)/A.cpp
+++ ICPC Collaborative Contest - 82 (ICPC Preli Edition)/A.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long N;
+    cin >> N;
+
+    if (N / 100 == 7 || (N / 10) % 10 == 7 || N % 10 == 7)
+    {
+
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
 
