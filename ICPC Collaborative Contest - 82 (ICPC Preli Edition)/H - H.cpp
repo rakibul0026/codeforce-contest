1240dc298bd300224d3c7706040fa1cdf5a35e31
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-18 20:04:09 +0600

    Update H - H.cpp

--- ICPC Collaborative Contest - 82 (ICPC Preli Edition)/H - H.cpp
+++ ICPC Collaborative Contest - 82 (ICPC Preli Edition)/H - H.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(2 * n);
+        for (int i = 0; i < 2 * n; ++i)
+        {
+            cin >> a[i];
+        }
+
+        sort(a.begin(), a.end(), greater<int>());
+
+        long long score = 0;
+
+        for (int i = 0; i < n; ++i)
+        {
+            score += a[i] - a[2 * n - i - 1];
+        }
+
+        cout << score << endl;
+    }
+    return 0;
+}
