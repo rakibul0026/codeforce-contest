6b0714bd122ffcdc5e411a840bc90179dc97a51d
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-19 23:07:16 +0600

    Update C. A TRUE Battle.cpp

--- Codeforces Round 979 (Div. 2)/C. A TRUE Battle.cpp
+++ Codeforces Round 979 (Div. 2)/C. A TRUE Battle.cpp
@@ -1,1 +1,30 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        if (s[0] == '1' || s[s.length() - 1] == '1' || s.find("11") != string::npos)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+
+    return 0;
+}
 
