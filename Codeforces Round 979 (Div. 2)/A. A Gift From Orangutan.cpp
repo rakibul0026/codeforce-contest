c06e3d10cff0f74124a223140ed509c88fda7246
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-19 23:08:16 +0600

    Update A. A Gift From Orangutan.cpp

--- Codeforces Round 979 (Div. 2)/A. A Gift From Orangutan.cpp
+++ Codeforces Round 979 (Div. 2)/A. A Gift From Orangutan.cpp
@@ -1,1 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
 
+ while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<int> v(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> v[i];
+           
+        }
+        sort(v.begin(), v.end());
+        int max_val=v[n-1];
+        int min_val=v[0];
+        int sum = (n-1)*(max_val-min_val);
+
+        
+        cout << sum<<endl;
+    }
+}
