40b6a64a9b904aabd4d9e0febddd19dfa6ed78e4
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-25 00:29:22 +0600

    Update B. Sakurako and Water.cpp

--- Codeforces Round 981 (Div. 3)/B. Sakurako and Water.cpp
+++ Codeforces Round 981 (Div. 3)/B. Sakurako and Water.cpp
@@ -1,1 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        vector<vector<int>> a(n, vector<int>(n));
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                cin >> a[i][j];
+            }
+        }
+
+        vector<int> diff(n * 2 - 1, 0);
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+
+                diff[i - j + n - 1] = min(diff[i - j + n - 1], a[i][j]);
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < diff.size(); i++)
+        {
+            if (diff[i] < 0)
+            {
+                ans -= diff[i];
+            }
+        }
+
+        cout << ans << endl;
+    }
+
+    return 0;
+}
 
