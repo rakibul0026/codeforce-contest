bffe18200b4548a6bb48f13b897d1f9f48857018
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-25 00:28:39 +0600

    Update A. Sakurako and Kosuke.cpp

--- Codeforces Round 981 (Div. 3)/A. Sakurako and Kosuke.cpp
+++ Codeforces Round 981 (Div. 3)/A. Sakurako and Kosuke.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+
+        cout << (n % 2 == 0 ? "Sakurako" : "Kosuke") << endl;
+    }
+
+    return 0;
+}
 
