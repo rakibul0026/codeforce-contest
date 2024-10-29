6a70d3077711eb7e0e68dc6b8e9e0d22987ed89c
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-10-29 17:30:41 +0600

    Update Problem E: Sub-Array.cpp

--- ICPC Dhaka Regional Preliminary Contest, 2023/Problem E: Sub-Array.cpp
+++ ICPC Dhaka Regional Preliminary Contest, 2023/Problem E: Sub-Array.cpp
@@ -1,1 +1,30 @@
-p
+#include <bits/stdc++.h>
+using namespace std;
+
+int32_t main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t, cs = 0;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n + 1];
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> a[i];
+        }
+        long long ans = 1LL * n * (n + 1) / 2;
+        for (int i = 1, j = 1; i <= n; i = j)
+        {
+            for (j = i + 1; j <= n and a[i] == a[j]; j++);
+                
+            int k = (j - i);
+            ans -= 1LL * k * (k + 1) / 2;
+        }
+        cout << "Case " << ++cs << ": " << ans << '\n';
+    }
+    return 0;
+}
