fbc859e0b56f31c727a68508c9368f514eb298f2
MD.Rakibul Hassan <152070403+rakibul0026@users.noreply.github.com>
2024-11-03 00:04:47 +0600

    Update A. Quintomania.cpp

--- Codeforces Round 984 (Div. 3)/A. Quintomania.cpp
+++ Codeforces Round 984 (Div. 3)/A. Quintomania.cpp
@@ -1,1 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[100];
+        
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+
+        bool found = true;
+        for (int i = 0; i < n - 1; i++) {
+            int diff =abs( arr[i + 1] - arr[i]);
+            if (diff != 5 && diff != 7) {
+                found = false;
+                break;
+            }
+        }
+
+        if (found) {
+            cout << "YES" << endl;
+        } else {
+            cout << "NO" << endl;
+        }
+    }
+}
 
